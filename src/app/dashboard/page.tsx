3cd7e59059c4621a643bbdc6a3072ddd7838fd2e
cepuii <54589413+cepuii@users.noreply.github.com>
2024-11-13 22:13:44 +0200

    add authentication clerk and prisma

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
+
+type Props = {};
+
+const DashboardPage = async (props: Props) => {
+  //Authentication
+  const auth = await onAuthenticateUser();
+  if (auth.status === 200 || auth.status === 201)
+    return redirect(`/dashboard/${auth.user?.workspace[0].id}`);
+
+  if (auth.status === 400 || auth.status === 500 || auth.status === 404) {
+    return redirect("/");
+  }
+};
+
+export default DashboardPage;
