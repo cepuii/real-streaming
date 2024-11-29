d7968ab80bae40e8cc6ebd0b07ea285a2e98e5a5
cepuii <54589413+cepuii@users.noreply.github.com>
2024-11-29 16:31:37 +0200

    add loading state into button

--- src/components/global/folders/folder.tsx
+++ src/components/global/folders/folder.tsx
@@ -53,7 +53,6 @@ const Folder = ({ id, name, count, optimistic }: Props) => {
     }
   };
 
-  //WIP add loading states
   return (
     <div
       onClick={handleFolderClick}
@@ -63,7 +62,7 @@ const Folder = ({ id, name, count, optimistic }: Props) => {
         "flex hover:bg-neutral-800 cursor-pointer transition duration-150 items-center gap-2 justify-between min-w-[250px] py-4 px-4 rounded-lg border-[1px]"
       )}
     >
-      <Loader state={false}>
+      <Loader state={isPending}>
         <div className="flex flex-col gap-[1px]">
           {onRename ? (
             <Input
